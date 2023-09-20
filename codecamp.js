05bb9b01c2f6264f60af05a736b42724ff6ebf46
Vladislav <kazakevich636@gmail.com>
2023-09-20 04:00:25 +0300

    Function Hi World

--- codecamp.js
+++ codecamp.js
@@ -1,12 +1,9 @@
 // CONDITION
 // Use the .shift() function to remove the first item from myArray and assign the "shifted off" value to a new variable, removedFromMyArray.
 
-
-
 // SOLUTION
 const myArray = [["John", 23], ["dog", 3]];
 const removedFromMyArray = myArray.shift();
-
 
 
 
@@ -16,3 +13,15 @@ const removedFromMyArray = myArray.shift();
 // SOLUTION
 const myList = [["Chocolate Bar", 15],["cake", 16],["cookie", 20],["candies", 25],
 ["apple", 22]];
+
+
+
+// CONDITION 
+// Create a function called reusableFunction which prints the string Hi World to the dev console.
+// Call the function.
+
+// SOLUTION
+function reusableFunction() {
+   console.log("Hi World");
+}
+reusableFunction();
