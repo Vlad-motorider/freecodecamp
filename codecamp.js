a833d514fb0f869d78766ec8b0cb28cc4c6579d0
Vladislav <kazakevich636@gmail.com>
2023-09-21 23:37:22 +0300

    Operator <

--- codecamp.js
+++ codecamp.js
@@ -25,3 +25,23 @@ function reusableFunction() {
    console.log("Hi World");
 }
 reusableFunction();
+
+
+
+// CONDITION 
+// Add the less than operator to the indicated lines so that the return statements make sense.
+
+// SOLUTION
+function testLessThan(val) {
+   if (val < 25) {  // Change this line
+      return "Under 25";
+   }
+
+   if (val < 55) {  // Change this line
+      return "Under 55";
+   }
+
+   return "55 or Over";
+}
+
+testLessThan(10);
