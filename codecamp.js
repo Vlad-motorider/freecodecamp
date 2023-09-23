8f577590f8713bd886c9b0e4ad27fabade8a32e8
Vladislav <kazakevich636@gmail.com>
2023-09-23 23:32:38 +0300

    testLogicalAnd

--- codecamp.js
+++ codecamp.js
@@ -2,66 +2,87 @@
 // Use the .shift() function to remove the first item from myArray and assign the "shifted off" value to a new variable, removedFromMyArray.
 
 // SOLUTION
-const myArray = [["John", 23], ["dog", 3]];
+const myArray = [
+  ["John", 23],
+  ["dog", 3],
+];
 const removedFromMyArray = myArray.shift();
-
-
 
 // CONDITION
 // There should be at least 5 sub-arrays in the list.
 
 // SOLUTION
-const myList = [["Chocolate Bar", 15],["cake", 16],["cookie", 20],["candies", 25],
-["apple", 22]];
+const myList = [
+  ["Chocolate Bar", 15],
+  ["cake", 16],
+  ["cookie", 20],
+  ["candies", 25],
+  ["apple", 22],
+];
 
-
-
-// CONDITION 
+// CONDITION
 // Create a function called reusableFunction which prints the string Hi World to the dev console.
 // Call the function.
 
 // SOLUTION
 function reusableFunction() {
-   console.log("Hi World");
+  console.log("Hi World");
 }
 reusableFunction();
 
-
-
-// CONDITION 
+// CONDITION
 // Add the less than operator to the indicated lines so that the return statements make sense.
 
 // SOLUTION
 function testLessThan(val) {
-   if (val < 25) {  // Change this line
-      return "Under 25";
-   }
+  if (val < 25) {
+    // Change this line
+    return "Under 25";
+  }
 
-   if (val < 55) {  // Change this line
-      return "Under 55";
-   }
+  if (val < 55) {
+    // Change this line
+    return "Under 55";
+  }
 
-   return "55 or Over";
+  return "55 or Over";
 }
 
 testLessThan(10);
 
-
-
-// CONDITION 
+// CONDITION
 // Add the less than or equal to operator to the indicated lines so that the return statements make sense.
 
 // SOLUTION
 function testLessOrEqual(val) {
-   if (val <= 12) {  // Change this line
-      return "Smaller Than or Equal to 12";
-   }
+  if (val <= 12) {
+    // Change this line
+    return "Smaller Than or Equal to 12";
+  }
 
-   if (val <= 24) {  // Change this line
-      return "Smaller Than or Equal to 24";
-   }
+  if (val <= 24) {
+    // Change this line
+    return "Smaller Than or Equal to 24";
+  }
 
-   return "More Than 24";
+  return "More Than 24";
 }
 
 testLessOrEqual(10);
+
+// CONDITION
+// Replace the two if statements with one statement, using the && operator, which will return the string Yes if val is less than or equal to 50 and greater than or equal to 25. Otherwise, will return the string No.
+
+// SOLUTION
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25) {
+    return "Yes";
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
