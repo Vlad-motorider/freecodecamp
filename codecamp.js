682a8c2ffe8babf1faddf4531cad46af5b40f899
Vladislav <kazakevich636@gmail.com>
2023-09-28 17:23:35 +0300

    testObj

--- codecamp.js
+++ codecamp.js
@@ -190,17 +190,13 @@ chainToSwitch(7);
 // CONDITION
 // Counting Cards
 // In the casino game Blackjack, a player can determine whether they have an advantage on the next hand over the house by keeping track of the relative number of high and low cards remaining in the deck. This is called Card Counting.
-
 // Having more high cards remaining in the deck favors the player. Each card is assigned a value according to the table below. When the count is positive, the player should bet high. When the count is zero or negative, the player should bet low.
-
 // Count Change	Cards
 // +1	2, 3, 4, 5, 6
 // 0	7, 8, 9
 // -1	10, 'J', 'Q', 'K', 'A'
 // You will write a card counting function. It will receive a card parameter, which can be a number or a string, and increment or decrement the global count variable according to the card's value (see table). The function will then return a string with the current count and the string Bet if the count is positive, or Hold if the count is zero or negative. The current count and the player's decision (Bet or Hold) should be separated by a single space.
-
 // Example Outputs: -3 Hold or 5 Bet
-
 // Hint
 // Do NOT reset count to 0 when value is 7, 8, or 9.
 // Do NOT return an array.
@@ -236,3 +232,20 @@ function cc(card) {
  }
  
  cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+
+
+// CONDITION
+//Read the values of the properties an entree and the drink of testObj using bracket notation and assign them to entreeValue and drinkValue respectively.
+
+// SOLUTION
+// Setup
+const testObj = {
+   "an entree": "hamburger",
+   "my side": "veggies",
+   "the drink": "water"
+ };
+ 
+ // Only change code below this line
+ const entreeValue = testObj['an entree'];   // Change this line
+ const drinkValue = testObj['the drink'];    // Change this line
