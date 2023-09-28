a8ce2dc9d67fc157892759e5b05104b3e85aa9ae
Vladislav <kazakevich636@gmail.com>
2023-09-28 17:57:17 +0300

    phoneticLookup

--- codecamp.js
+++ codecamp.js
@@ -249,3 +249,28 @@ const testObj = {
  // Only change code below this line
  const entreeValue = testObj['an entree'];   // Change this line
  const drinkValue = testObj['the drink'];    // Change this line
+
+
+
+ // CONDITION
+ //Convert the switch statement into an object called lookup. Use it to look up val and assign the associated string to the result variable.
+
+// SOLUTION
+function phoneticLookup(val) {
+   let result = "";
+ 
+   const lookup = {
+     'alpha': "Adams",
+     'bravo': "Boston",
+     'charlie': "Chicago",
+     'delta': "Denver",
+     'echo': "Easy",
+     'foxtrot': "Frank"
+   }
+   
+   result = lookup[val];
+ 
+   return result;
+ }
+ 
+ phoneticLookup("charlie");
