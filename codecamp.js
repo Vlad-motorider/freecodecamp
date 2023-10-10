d5523d0c37d05df3edf3501cdc4690953a4c0710
Vladislav <kazakevich636@gmail.com>
2023-10-10 21:14:59 +0300

    Countdown

--- codecamp.js
+++ codecamp.js
@@ -367,3 +367,23 @@ function multiplyAll(arr) {
  }
  
  multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+
+
+
+// CONDITION
+// We have defined a function called countdown with one parameter (n). The function should use recursion to return an array containing the integers n through 1 based on the n parameter.
+// If the function is called with a number less than 1, the function should return an empty array. For example, calling this function with n = 5 should return the array [5, 4, 3, 2, 1].
+// Your function must use recursion by calling itself and must not use loops of any kind.
+
+// SOLUTION
+// Only change code below this line
+function countdown(n){
+   if(n < 1) {
+      return [];
+   } else {
+      let arrNum = countdown(n - 1);
+      arrNum.unshift(n)
+      return arrNum;
+   }
+}
+ // Only change code above this line
